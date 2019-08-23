12f090dd2ee065fc3202a87cfe9203d1479abd5e
nizar <nizarucsc@gmail.com>
2019-08-23 14:18:39 +0530

    add user indentity model

---
+++ app/Models/User_identity.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User_identity extends Base_Model  {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_identities';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['identity_type_id', 'number', 'issue_date', 'expiry_date', 'issue_location', 'comments', 'security_user_id', 'modified_user_id', 'modified', 'created_user_id', 'created'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['modified', 'created', 'issue_date', 'expiry_date', 'modified', 'created'];
+
+}
---
+++ app/Models/User_nationality.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User_nationality extends Base_Model  {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_nationalities';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nationality_id', 'security_user_id', 'comments', 'preferred', 'modified_user_id', 'modified', 'created_user_id', 'created'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['modified', 'created'];
+
+}
