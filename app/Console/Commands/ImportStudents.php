97399ad4b1225c074795c5b09fe6738eb6c170e9
Mohamed Nizar <nizarucsc@gmail.com>
2019-11-05 03:03:05 +0530

    fix read error meesage issue

--- app/Console/Commands/ImportStudents.php
+++ app/Console/Commands/ImportStudents.php
@@ -414,7 +414,7 @@ class ImportStudents extends Command
             $reader = $this->setReader($file);
             $reader->setActiveSheetIndexByName($sheet);
             if(gettype($failures) == 'array' || 'object'){
-                $failures = array_map(array($this,'processErrors'),(array)$failures );
+                $failures = array_map(array($this,'processErrors'),$failures );
                 array_walk($failures , 'append_errors_to_excel',$reader);
                 $objWriter = $this->getSheetWriter($file,$reader);
                 Storage::disk('local')->makeDirectory('sis-bulk-data-files/processed');
