869b0ee8653673e439b086946ea5683c1f77d54e
Mohamed Nizar <nizarucsc@gmail.com>
2019-11-05 02:12:39 +0530

    Fix failures not a func on the method used to import

--- app/Console/Commands/ImportStudents.php
+++ app/Console/Commands/ImportStudents.php
@@ -259,7 +259,8 @@ class ImportStudents extends Command
                     case 1;
                         if (($this->getSheetName($file,'Insert Students')) && ($this->getHigestRow($file, $sheet,$column) > 0))  { //
                             $import = new UsersImport($file);
-                            Excel::import($import, $excelFile, 'local',$this->getSheetType($file['filename']));
+                            $import->import($excelFile,'local',$this->getSheetType($file['filename']));
+//                            Excel::import($import, $excelFile, 'local',$this->getSheetType($file['filename']));
                             DB::table('uploads')
                                 ->where('id', $file['id'])
                                 ->update(['insert' => 1,'is_processed' => 1,'updated_at' => now()]);
@@ -284,7 +285,8 @@ class ImportStudents extends Command
                     case 2;
                         if (($this->getSheetName($file,'Update Students')) && ($this->getHigestRow($file, $sheet,$column) > 0)) {
                             $import = new StudentUpdate($file);
-                            Excel::import($import, $excelFile, 'local',$this->getSheetType($file['filename']));
+                            $import->import($excelFile,'local',$this->getSheetType($file['filename']));
+//                            Excel::import($import, $excelFile, 'local',$this->getSheetType($file['filename']));
                             DB::table('uploads')
                                 ->where('id', $file['id'])
                                 ->update(['update' => 1,'is_processed' => 1,'updated_at' => now()]);
