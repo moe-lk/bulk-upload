c14e83512095c70634758da2c782942989a63d82
Mohamed Nizar <nizarucsc@gmail.com>
2019-11-04 23:06:34 +0530

    fix file loading

--- app/Console/Commands/ImportStudents.php
+++ app/Console/Commands/ImportStudents.php
@@ -200,7 +200,7 @@ class ImportStudents extends Command
     }
 
     protected function getType($file){
-        $file =  storage_path() . '/app/sis-bulk-data-files/'.$file; 
+        $file =  storage_path() . '/app/sis-bulk-data-files/'.$file;
         $inputFileType =  \PhpOffice\PhpSpreadsheet\IOFactory::identify($file);
         return $inputFileType;
     }
@@ -322,9 +322,8 @@ class ImportStudents extends Command
             $excelFile =  "sis-bulk-data-files/" . $file['filename'];
         }
         $excelFile = storage_path()."/app/" . $excelFile;
-        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($this->getType($file['filename']));
-        $objPHPExcel =  $reader->load($excelFile);
-        return $objPHPExcel;
+        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::load($excelFile);
+        return $reader;
     }
 
     protected function  getSheetName($file,$sheet){
