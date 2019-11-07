7cbf8f6b70b8e7c562c0b3ff225193340b6268d4
Mohamed Nizar <nizarucsc@gmail.com>
2019-11-07 09:12:04 +0530

    Partial errors issue

--- app/Console/Commands/ImportStudents.php
+++ app/Console/Commands/ImportStudents.php
@@ -268,7 +268,7 @@ class ImportStudents extends Command
                             DB::table('uploads')
                                 ->where('id', $file['id'])
                                 ->update(['insert' => 1,'is_processed' => 1,'updated_at' => now()]);
-                            if(!empty((array)$import->failures())){
+                            if($import->failures()->count() > 0){
                                 self::writeErrors($import,$file,'Insert Students');
                                 DB::table('uploads')
                                     ->where('id', $file['id'])
@@ -294,7 +294,7 @@ class ImportStudents extends Command
                             DB::table('uploads')
                                 ->where('id', $file['id'])
                                 ->update(['update' => 1,'is_processed' => 1,'updated_at' => now()]);
-                            if(!empty((array)$import->failures())){
+                            if($import->failures()->count() > 0){
                                 self::writeErrors($import,$file,'Update Students');
                                 DB::table('uploads')
                                     ->where('id', $file['id'])
