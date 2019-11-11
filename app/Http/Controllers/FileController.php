aaad8e269616239460b2639b68eee02353626b8e
Mohamed Nizar <nizarucsc@gmail.com>
2019-11-11 11:54:08 +0530

    ad new tamplat version 2007_V1.5_20191111.xlsx

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -74,7 +74,7 @@ class FileController extends Controller
 
     public function downloadTemplate(){
         $filename = 'censusNo_className_sis_students_bulk_upload';
-        $version = '2007_V1.4_20191107.xlsx';
+        $version = '2007_V1.5_20191111.xlsx';
         $file_path = storage_path() .'/app/public/'. $filename.'_'.$version;;
         if (file_exists($file_path))
         {
