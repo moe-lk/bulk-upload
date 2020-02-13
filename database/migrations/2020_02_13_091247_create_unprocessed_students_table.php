63ceb5d0164ee5ae960eda1deee1f4b47f35463f
janithRS <janithravindu@gmail.com>
2020-02-13 16:07:18 +0530

    Create Unprocessed student Model and the Migration and update the student processing (handle method) in RunAddApprovedStudents class

---
+++ database/migrations/2020_02_13_091247_create_unprocessed_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUnprocessedStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unprocessed_students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('current_unprocessed_students_count');
+            $table->tinyInteger('is_processed');
+            $table->tinyInteger('notification');
+            $table->integer('institution_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unprocessed_students');
+    }
+}
