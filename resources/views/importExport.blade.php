0afaacc6f5dc968e9da94a87384b31ac8b4ccf04
janithRS <janithravindu@gmail.com>
2020-02-13 08:10:27 +0530

    (#134) Add an alert in bulk upload dashboard

--- resources/views/importExport.blade.php
+++ resources/views/importExport.blade.php
@@ -6,12 +6,12 @@
           <h1>NEMIS - SIS Bulk data import</h1>
           </div>
           <div class="panel-body">
-{{--              <iframe width="1200" height="450" frameborder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=367F7CD71188D7DA%211012&authkey=%21AL4A-jLv8V-fhEI&em=2&wdAllowInteractivity=False&AllowTyping=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True"></iframe>--}}
-
-
-            <a href="https://onedrive.live.com/download?resid=367F7CD71188D7DA%211012&authkey=%21AL4A-jLv8V-fhEI&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True" target="_blank"><button class="btn btn-success"> Download Template</button></a>
+              <div class="alert alert-dismissable alert-danger" role="alert">
+                  <strong>Notice!</strong> The email processing is temporary paused due to a system upgrade.
+              </div>
+              <a href="https://onedrive.live.com/download?resid=367F7CD71188D7DA%211012&authkey=%21AL4A-jLv8V-fhEI&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True" target="_blank"><button class="btn btn-success"> Download Template</button></a>
               <a href="{{ url('downloadExcel') }}"><button class="btn btn-success">Download Template for  Excel2007</button></a>
-            <form style="border: 4px solid #a1a1a1;margin-top: 15px;padding: 10px;" action="{{ url('upload') }}" class="form-horizontal" method="post" enctype="multipart/form-data">
+              <form style="border: 4px solid #a1a1a1;margin-top: 15px;padding: 10px;" action="{{ url('upload') }}" class="form-horizontal" method="post" enctype="multipart/form-data">
                 @csrf
 
                 @if ($errors->any())
@@ -39,11 +39,11 @@
                 </select>
                 <input type="file" name="import_file" required />
                 <button class="btn btn-primary">Import File</button>
-            </form>
+              </form>
 <!--
                  <form style="border: 4px solid #a1a1a1;margin-top: 15px;padding: 10px;" action="{{ url('exportExcel') }}" class="form-horizontal" method="post" enctype="multipart/form-data">
                 @csrf
- 
+
                 @if ($errors->any())
                     <div class="alert alert-danger">
                         <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
@@ -54,7 +54,7 @@
                         </ul>
                     </div>
                 @endif
- 
+
                 @if (Session::has('success'))
                     <div class="alert alert-success">
                         <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
