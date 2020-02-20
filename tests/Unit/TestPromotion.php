e9f1741bef43c34105148e4b5a6de2a0362c5091
janithRS <janithravindu@gmail.com>
2020-02-20 14:40:44 +0530

    (moe-lk/sis-php#146) Initialize Promotion test cases

--- tests/Unit/TestPromotion.php
+++ tests/Unit/TestPromotion.php
@@ -2,20 +2,40 @@
 
 namespace Tests\Unit;
 
+use App\Console\Commands\PromoteStudents;
+use App\Models\Education_grade;
+use App\Models\Institution_grade;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TestPromotion extends TestCase
 {
-    /**
-     * Check the command exiting
-     *
-     * @return void
-     */
-    public function testCommand()
+    public $promoteStudents ;
+    private $instituion_grade;
+    private $education_grades;
+
+    protected function setUp(): void
     {
-        $this->artisan('promote:students 2020')
-            ->assertExitCode(0);
+        parent::setUp();
+        $this->promoteStudents = new PromoteStudents();
+        $this->instituion_grade = new Institution_grade();
+        $this->education_grades = new Education_grade();
+    }
+
+    public function testPromotionParallelProcess()
+    {
+        $institutionGrade = $this->instituion_grade->getInstitutionGradeToPromoted('2020','9861');
+        $educationGrades = $this->education_grades->getNextGrade($institutionGrade->education_grade_id);
+        $promoteProcess = $this->promoteStudents->process($institutionGrade, $educationGrades, 2019);
+        $this->assertEquals(2, $promoteProcess);
+    }
+
+    public function testPromotionPoolProcess()
+    {
+        //todo : build test cases according to the school entries
+    }
+
+    public function testPromotionGraduationProcess()
+    {
+        //todo : build test cases according to the school entries
     }
 }
