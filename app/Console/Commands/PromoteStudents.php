50d6aa82c4e1bdbad8d092c289d1ce8db15455a2
janithRS <janithravindu@gmail.com>
2020-02-27 14:45:43 +0530

    Change return values in promote process method

--- app/Console/Commands/PromoteStudents.php
+++ app/Console/Commands/PromoteStudents.php
@@ -159,7 +159,7 @@ class PromoteStudents extends Command
                 }elseif (($nextGradeObj->count() > 1) && ($nextGradeObj->count() !==  $currentGradeObj->count())){
                     // promote pool promotion
                     $this->promotion($institutionGrade,$nextGrade,$academicPeriod,$nextAcademicPeriod,[],1);
-                    return 1;
+                    return 2;
                 }elseif(($nextGradeObj->count() > 1) && $currentGradeObj->count() == $nextGradeObj->count()){
                     // Promote matching class name with previous class
                     $this->promotion($institutionGrade,$nextGrade,$academicPeriod,$nextAcademicPeriod,$nextGradeObj->toArray(),1);
@@ -167,12 +167,12 @@ class PromoteStudents extends Command
                 }else{
                     // default pool promotion
                     $this->promotion($institutionGrade,$nextGrade,$academicPeriod,$nextAcademicPeriod,[],1);
-                    return 1;
+                    return 2;
                 }
             }else{
                 // default pool promotion
                 $this->promotion($institutionGrade,$nextGrade,$academicPeriod,$nextAcademicPeriod,[],3);
-                return 1;
+                return 2;
             }
         }
 
