98b6bb6187e4c8792d6f7be151afd15d23a49bcc
Mohamed Nizar <nizarucsc@gmail.com>
2020-04-09 11:45:43 +0530

    Update ImportStudents.php

--- app/Console/Commands/ImportStudents.php
+++ app/Console/Commands/ImportStudents.php
@@ -126,6 +126,7 @@ class ImportStudents extends Command
                 'uploads.updated_at', 'uploads.is_email_sent', 'uploads.update', 'uploads.insert', 'uploads.node')
             ->join('user_contacts', 'uploads.security_user_id', '=', 'user_contacts.security_user_id')
             ->join('contact_types', 'user_contacts.contact_type_id', '=', 'contact_types.id')
+            //only for UAT
             ->where('contact_types.contact_option_id', '=', 6)
             ->limit(1)
             ->get()->toArray();
