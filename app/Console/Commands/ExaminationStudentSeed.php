5e14f38363237346afdf07e9b98d81d5396f1106
Nizar <nizarucsc@gmail.com>
2020-06-02 16:00:12 +0530

    Add deleted seed command

---
+++ app/Console/Commands/ExaminationStudentSeed.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\ExaminationStudentsController;
+use Illuminate\Console\Command;
+
+class ExaminationStudentSeed extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'examination:migration';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command to seed from  the examination data (csv) to sis deticated examination table';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $output = new \Symfony\Component\Console\Output\ConsoleOutput();
+        $output->writeln('###########################################------Inserting file records------###########################################');
+        ExaminationStudentsController::callOnClick();
+        $output->writeln('###########################################------Finished inserting file records------###########################################');
+    }
+}
