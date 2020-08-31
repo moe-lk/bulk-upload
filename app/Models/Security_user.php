fc1ed129ef03b9827f065d9fab3f7164066073cd
Nizar <nizarucsc@gmail.com>
2020-08-31 15:04:39 +0530

    fix undefined exmintion_g5 filed

--- app/Models/Security_user.php
+++ app/Models/Security_user.php
@@ -217,6 +217,7 @@ class Security_user extends Model
         try {
             self::where( 'id'  , $sis_student['student_id'])->update($studentData);
             $studentData['id'] = $sis_student['student_id'];
+            unset($studentData['examination_students']);
             $this->uniqueUserId->updateOrInsertRecord($studentData);
             return $studentData;
         } catch (\Exception $th) {
