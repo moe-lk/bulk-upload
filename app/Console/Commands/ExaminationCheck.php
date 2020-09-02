1015d251411fc5fbb62fbb53a2da71d0f0784848
Nizar <nizarucsc@gmail.com>
2020-09-02 11:09:58 +0530

    fix offset missing

--- app/Console/Commands/ExaminationCheck.php
+++ app/Console/Commands/ExaminationCheck.php
@@ -44,6 +44,7 @@ class ExaminationCheck extends Command
         $count = DB::table('examination_students')->select('nsid')->distinct()->count();
         $studentsIdsWithDuplication =   DB::table('examination_students as es')
         ->select(DB::raw('count(*) as total'),'es.*')
+        ->whereNotNull('es.nsid')
         ->having('total','>',1)
         ->groupBy('es.nsid')
         ->orderBy('es.nsid')
