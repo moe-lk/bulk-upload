8b43534a69d232c82a043a321bf653d38c6dbbc0
Mohamed Nizar <nizaucsc@gmail.com>
2020-11-03 12:09:27 +0530

    fix class create multiple issue

--- app/Models/Institution_class.php
+++ app/Models/Institution_class.php
@@ -82,14 +82,16 @@ class Institution_class extends Base_Model
 
         if ($al == true) {
             $query->where('education_programmes.education_cycle_id', 4)
-            ->where('institution_id', $shift);
+            ->where('institution_id', $shift['institution_id'])
+            ->where('institution_shift_id', $shift['id']);
             $data = $query
             ->groupBy('institution_classes.id')
             ->get()->toArray();
             return $data;
         } else {
             $query->where('education_programmes.education_cycle_id','<>',4)
-            ->where('institution_shift_id', $shift);
+            ->where('institution_id', $shift['institution_id'])
+            ->where('institution_shift_id', $shift['id']);
             $data = $query
             ->groupBy('institution_classes.id')
             ->get()->toArray();
