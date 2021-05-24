5df20ab20b1da33fb28a524bb600f58d2a4f6962
HarindaVithana <harindavithana@gmail.com>
2021-05-24 09:48:51 +0530

    Update ExaminationStudentsController.php

--- app/Http/Controllers/ExaminationStudentsController.php
+++ app/Http/Controllers/ExaminationStudentsController.php
@@ -485,7 +485,7 @@ class ExaminationStudentsController extends Controller
 
     public function downloadProcessedFile()
     {
-        $file_path = storage_path() . '/app/examination/student_data_with_nsid.1616052465.csv';
+        $file_path = storage_path() . '/app/examination/student_data_with_nsid.1616052475.csv';
         return Response::download($file_path);
     }
 }
